19c594250392260cd83914a84a44631cc53ec239
Geno McNew <genemcnew3@gmail.com>
2020-06-28 00:36:55 -0500

    cards print with pins when logged in, without when logged out

--- src/javascripts/components/body/body.js
+++ src/javascripts/components/body/body.js
@@ -1,17 +1,17 @@
 import auth from '../auth/auth';
+import cardsList from '../cardsList/cardsList';
 
 const bodyStart = () => {
   const greetDiv = $('#greet');
-  const cardsDiv = $('#cards');
   const boardsDiv = $('#boards');
   const user = auth.getUser();
   if (user === null) {
     greetDiv.text('Hello Guest! Log in For your personalized experience');
-    cardsDiv.html('');
+    cardsList.cardHTML();
     boardsDiv.html('<h2>Log in to see your boards here</h2>');
   } else {
     greetDiv.text(`Hello ${user.displayName}!`);
-    cardsDiv.html('');
+    cardsList.cardHTML();
     boardsDiv.html('<h1>Your Board will be here</h1>');
   }
 };
