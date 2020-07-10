36554749d69a1583675482a6b5ff2c7a948cbc23
Geno McNew <genemcnew3@gmail.com>
2020-07-09 20:39:37 -0500

    can add new pins

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-
+import 'bootstrap';
 import '../styles/main.scss';
 import navbar from './components/navbar/navbar';
 import apikeys from './helpers/apiKeys.json';
