70878d786744e2ffe684d7ae430da684d1678da8
Geno McNew <genemcnew3@gmail.com>
2020-07-09 22:57:40 -0500

    wip

--- src/javascripts/components/addBoard/addBoard.js
+++ src/javascripts/components/addBoard/addBoard.js
@@ -1,6 +1,12 @@
 const addBoard = () => {
   const boardName = $('#newBoard').val();
-  console.warn(boardName);
+  let duplicate = false;
+  $('.boards').each((index, element) => {
+    if (boardName === $(element).text()) {
+      duplicate = true;
+    }
+  });
+  console.warn(duplicate);
 };
 
 export default { addBoard };
